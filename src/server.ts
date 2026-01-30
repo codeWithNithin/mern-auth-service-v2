4aedc6eefabb9287019c0b476689222384734e72
codeWithNithin <nithinkottary25@gmail.com>
2026-01-30 15:54:05 +0530

    install express and setup server

--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,17 @@
+import app from './app.js';
 import { Config } from './config/index.js';
 
-console.log(Config.PORT);
+const startServer = () => {
+    const { PORT } = Config;
+
+    try {
+        app.listen(PORT, () => {
+            console.log(`Server running at ${PORT}`);
+        });
+    } catch (err) {
+        console.log(err);
+        process.exit(1);
+    }
+};
+
+void startServer();
