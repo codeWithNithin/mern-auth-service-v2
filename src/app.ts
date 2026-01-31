11b1f6086cf91f5093bdac246a04697e069508e0
codeWithNithin <nithinkottary25@gmail.com>
2026-01-31 13:37:23 +0530

    add global err handler

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,6 @@
-import express from 'express';
+import express, { NextFunction, Request, Response } from 'express';
+import logger from './config/logger.js';
+import type { HttpError } from 'http-errors';
 
 const app = express();
 
@@ -6,4 +8,24 @@ app.get('/', (req, res) => {
     res.send('Hello world !');
 });
 
+/**
+ * Global Error handler
+ */
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+app.use((err: HttpError, req: Request, res: Response, next: NextFunction) => {
+    logger.error(err.message);
+    const statusCode = err.statusCode || 500;
+
+    res.status(statusCode).json({
+        errors: [
+            {
+                type: err.name,
+                message: err.message,
+                path: '',
+                location: '',
+            },
+        ],
+    });
+});
 export default app;
