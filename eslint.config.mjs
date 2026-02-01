40192906cd741947eec7aefbdef1c70ffe5e6804
codeWithNithin <nithinkottary25@gmail.com>
2026-02-01 16:41:24 +0530

    add docker container

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -16,7 +16,7 @@ export default defineConfig(
         rules: {
             'no-console': 'off',
             'dot-notation': 'error',
-             '@typescript-eslint/no-unsafe-assignment': 'off',
+            '@typescript-eslint/no-unsafe-assignment': 'off',
         },
     },
     {
